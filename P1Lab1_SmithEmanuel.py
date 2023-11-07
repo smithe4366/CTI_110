0e1de11c197d68f5ebfaca7fd8707d033966a8fa
smithe4366 <150168959+smithe4366@users.noreply.github.com>
2023-11-07 11:21:53 -0500

    First Upload

---
+++ P1Lab1_SmithEmanuel.py
@@ -0,0 +1,9 @@
+#Emanuel Smith
+#11/27/2023
+#Basic Input/Output
+
+#Get input from user
+name = input ("Please enter your name:  ")
+
+#Display output to user
+print("Hello", name, "welcome to CTI-110!")
